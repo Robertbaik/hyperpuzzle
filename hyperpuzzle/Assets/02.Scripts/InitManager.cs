cd42e663ea0770161eef16ca2c7cf4a1eb12547c
robertbaik <robertbbaik@gmail.com>
2020-07-07 00:08:54 +0900

    puzzle init

---
+++ hyperpuzzle/Assets/02.Scripts/InitManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Node
+{
+    public int xPos;
+    public int yPos;
+
+    public int idx;
+
+    public Node rightNode;
+    public Node leftNode;
+
+    public Node(int i)
+    {
+        idx = i;
+    }
+}
+
+public class InitManager : MonoBehaviour
+{
+    public int gameLevel;
+    public GameObject blockBtn;
+    void Start()
+    {
+        InitGameLevel(gameLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitGameLevel(int level)
+    {
+        for (int i = 0; i < level*level; i++)
+        {
+            Node node = new Node(i);
+            InitBtnObj(node);
+        }
+    }
+
+    public void InitBtnObj(Node node)
+    {
+
+    }
+
+}
