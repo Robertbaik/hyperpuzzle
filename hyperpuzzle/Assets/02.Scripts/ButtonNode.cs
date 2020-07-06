c68a58e5091bfa4dbe2f9998d372424215273fcb
robertbaik <robertbbaik@gmail.com>
2020-07-07 00:25:44 +0900

    DoTween 추가

---
+++ hyperpuzzle/Assets/02.Scripts/ButtonNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonNode : MonoBehaviour
+{
+    public int idx;
+
+    public int xPos;
+    public int yPos;
+
+    public ButtonNode rightNode = null;
+    public ButtonNode leftNode = null;
+
+    public Button btn;
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(Onclick_Button);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Onclick_Button()
+    {
+        transform.DOMoveX(2, 1);
+        Debug.Log($"Onclick : {transform.name}");
+    }
+}
